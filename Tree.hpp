2d8a34b0003e3bd318555a8e8ba17e519eab1f8c
peleg122 <peleg122@gmail.com>
2019-03-19 19:31:43 +0200

    Update Tree.hpp

--- Tree.hpp
+++ Tree.hpp
@@ -10,44 +10,44 @@
 // Created by Peleg on 15/03/2019.
 //
 namespace ariel{
-class Tree{
-private:
-    int counter = 0;
-    struct TreeNode{
-        int data;
-        TreeNode* child_left;
-        TreeNode* child_right;
-    };
-    TreeNode* root_node;
-    void insertPrivate(TreeNode* node ,int data);
-    void printInOrderPrivate(TreeNode* node);
-    TreeNode* parentPrivate(TreeNode* node ,int data);
-    int leftPrivate(TreeNode* node, int data);
-    int rightPrivate(TreeNode* node, int data);
-    void removePrivate(TreeNode* node, int data);
-    void removeRootData();
-    void removeData(TreeNode* node, TreeNode* removeNode, bool left);
-    int SmallestPrivate(TreeNode* node);
-    void removeRoot(TreeNode* node);
-    int sizePrivate();
-    TreeNode* target(TreeNode* node, int data);
-    bool containsPrivate(TreeNode* node,int data);
-    int rootPrivate();
-public:
-    Tree();
-    ~Tree();
-    TreeNode* Create_Node(int data);
-    void insert(int data);
-    void print();
-    TreeNode* parentChild(int data);
-    int parent(int data);
-    int left(int data);
-    int right(int data);
-    void remove(int data);
-    int Smallest();
-    int size();
-    bool contains(int data);
-    int root();
+    class Tree{
+    private:
+        int counter = 0;
+        struct TreeNode{
+            int data;
+            TreeNode* child_left;
+            TreeNode* child_right;
+        };
+        TreeNode* root_node;
+        void insertPrivate(TreeNode* node ,int data);
+        void printInOrderPrivate(TreeNode* node);
+        TreeNode* parentPrivate(TreeNode* node ,int data);
+        int leftPrivate(TreeNode* node, int data);
+        int rightPrivate(TreeNode* node, int data);
+        void removePrivate(TreeNode* node, int data);
+        void removeRootData();
+        void removeData(TreeNode* node, TreeNode* removeNode, bool left);
+        int SmallestPrivate(TreeNode* node);
+        void removeRoot(TreeNode* node);
+        int sizePrivate();
+        TreeNode* target(TreeNode* node, int data);
+        bool containsPrivate(TreeNode* node,int data);
+        int rootPrivate();
+    public:
+        Tree();
+        ~Tree();
+        TreeNode* Create_Node(int data);
+        void insert(int data);
+        void print();
+        TreeNode* parentChild(int data);
+        int parent(int data);
+        int left(int data);
+        int right(int data);
+        void remove(int data);
+        int Smallest();
+        int size();
+        bool contains(int data);
+        int root();
 
+    };
 };
-}
